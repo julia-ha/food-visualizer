7dd77e5e6f76ddd061057f774c8a8ebe4b32a9d5
julia ha <jjh497@nyu.edu>
2016-11-30 17:58:33 -0500

    config.json

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose'),
 	URLSlugs = require('mongoose-url-slugs');
 
+
 /*
 var Image = new mongoose.Schema({
 	caption: String,
@@ -45,4 +46,24 @@ mongoose.model('Item', Item);
 mongoose.model('List', List);
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/finalproject');
+//mongoose.connect('mongodb://localhost/finalproject');
+
+// is the environment variable, NODE_ENV, set to PRODUCTION? 
+if (process.env.NODE_ENV == 'PRODUCTION') {
+ // if we're in PRODUCTION mode, then read the configration from a file
+ // use blocking file io to do this...
+ var fs = require('fs');
+ var path = require('path');
+ var fn = path.join(__dirname, 'config.json');
+ var data = fs.readFileSync(fn);
+
+ // our configuration file will be in json, so parse it and set the
+ // conenction string appropriately!
+ var conf = JSON.parse(data);
+ var dbconf = conf.dbconf;
+} else {
+ // if we're not in PRODUCTION mode, then use
+ dbconf = 'mongodb://localhost/finalproject';
+}
+
+mongoose.connect(dbconf);
