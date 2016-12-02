1bf2fc4b03c71ae4b9010b20af02b015a5ba9a75
julia ha <jjh497@nyu.edu>
2016-12-01 23:06:28 -0500

    fixed formatting

--- db.js
+++ db.js
@@ -4,34 +4,6 @@ var passportLocalMongoose = require('passport-local-mongoose');
 
 
 
-
-/*
-var UserSchema = new mongoose.Schema({
-  // username, password provided by Passport plugin
-  list: [{type: mongoose.Schema.Types.ObjectId, ref: 'List' }],
-  //results: [{type: mongoose.Schema.Types.ObjectId, ref: 'Results'}]
-});
-
-//Items
-//user is only responsible for adding the name of the food
-//when posted to user's list of items, group will be assigned by web app
-//(possibly using a food database from USDA or similar) to determine food group of item
-var Item = new mongoose.Schema({
-    food: {type: String, required: true},
-    group:{type: String}
-});
-
-//List
-//keeps track of all items added by user
-var List = new mongoose.Schema({
-    //user: {type: mongoose.Schema.Types.ObjectId, ref:'User'},
-    //createdAt: {type: Date},
-    user: {type: mongoose.Schema.Types.ObjectID, ref: 'User'},
-    items: [Item]
-});
-
-*/
-
 var UserSchema = new mongoose.Schema({
   // username, password provided by Passport plugin
   items: [{type: mongoose.Schema.Types.ObjectId, ref:'Item'}]
@@ -42,18 +14,6 @@ var Item = new mongoose.Schema({
   user: {type: mongoose.Schema.Types.ObjectId, ref:'User'},
   food: {type: String, required: true}
 });
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
